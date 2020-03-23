f594a42115a1c611320eff214a27c9e7a2662122
zhuzhihai <zhuzhihai@mail.wjhudong.com>
2020-03-24 00:41:08 +0800

    修改mapper.xml文件存放位置

--- exam/src/main/java/com/moyulab/cn/exam/system/entity/SysUser.java
+++ exam/src/main/java/com/moyulab/cn/exam/system/entity/SysUser.java
@@ -1,11 +1,13 @@
 package com.moyulab.cn.exam.system.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class SysUser {
+    @TableId
     private Long userId;
     private String userName;
     private String password;
